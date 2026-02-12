7491ebe68f078630c200e4edf6cafcaf9320a7a1
AshiquzzamanCse <ashiquzzaman.rajib.cse@gmail.com>
2026-02-12 21:14:40 +0600

    Update By Employee Mail System 12-02-2026

--- resources/views/admin/pages/apply/post.blade.php
+++ resources/views/admin/pages/apply/post.blade.php
@@ -100,27 +100,6 @@
     </style>
 
     <div class="card card-flash">
-        <div class="card-header mt-6">
-            <div class="card-title"></div>
-            <div class="card-toolbar">
-
-                {{-- <a href="{{ route('admin.banner.create') }}" class="btn btn-light-primary">
-                    <span class="svg-icon svg-icon-3">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                            fill="none">
-                            <rect opacity="0.3" x="2" y="2" width="20" height="20" rx="5"
-                                fill="currentColor" />
-                            <rect x="10.8891" y="17.8033" width="12" height="2" rx="1"
-                                transform="rotate(-90 10.8891 17.8033)" fill="currentColor" />
-                            <rect x="6.01041" y="10.9247" width="12" height="2" rx="1"
-                                fill="currentColor" />
-                        </svg>
-                    </span>
-                    Add Banner
-                </a> --}}
-
-            </div>
-        </div>
 
         <div class="card-body pt-0">
             <table id="kt_datatable_example_5" class="table table-striped table-row-bordered gy-5 gs-7 border rounded">
